da8265baa96a854388cbe063579f138fed3819f7
fat-Ruud <53981601+fat-Ruud@users.noreply.github.com>
2020-05-15 20:06:07 -0500

    89uiprimer commit
    se agregó las clases de archivo de clientes, clienteDao, parejas y manejo de parejas

--- src/co/edu/unbosque/model/Mundo.java
+++ src/co/edu/unbosque/model/Mundo.java
@@ -1,31 +1,11 @@
 package co.edu.unbosque.model;
 
-import co.edu.unbosque.model.persistence.Binaries;
-import co.edu.unbosque.model.persistence.GabenFile;
-import co.edu.unbosque.model.persistence.GabenProperties;
+
 
 /**
  * @author Gabriel Blanco
  * Clase "multitoma" donde se define la solución de la problemática
  */
 public class Mundo {
-	private GabenFile archivo = new GabenFile();
-	private GabenProperties propiedad = new GabenProperties();
-	private Binaries binarios = new Binaries();
 	
-	public String toString() {
-		return "Buena Hackerman, crack, leyenda";
-	}
-
-	public GabenFile getArchivo() {
-		return archivo;
-	}
-
-	public GabenProperties getPropiedad() {
-		return propiedad;
-	}
-
-	public Binaries getBinarios() {
-		return binarios;
-	}
 }
